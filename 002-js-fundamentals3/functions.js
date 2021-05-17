8a3b0deb3176b070f0a89fd1a6860e99b57ceac1
Andy Hall <andersonhall@gmail.com>
2021-05-16 19:50:48 -0500

    js fundamentals

---
+++ 002-js-fundamentals3/functions.js
@@ -0,0 +1,15 @@
+function add7(n) {
+  return n + 7;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function lastLetter(str) {
+  return str.charAt(str.length - 1);
+}
