27e48d980bb1e22073b54efd576de2bb2877a00d
Andy Hall <andersonhall@gmail.com>
2021-05-29 10:00:03 -0500

    slider works

--- 018-dynamic-ui/imageSlider.js
+++ 018-dynamic-ui/imageSlider.js
@@ -2,24 +2,31 @@ const imageSlider = (() => {
   const container = document.querySelector('.img-container-inner');
   const prevBtn = document.querySelector('.left');
   const nextBtn = document.querySelector('.right');
+  const navDotsContainer = document.querySelector('.nav-dots');
 
   let index = 1;
   let translateX = 0;
 
   const previousSlide = () => {
     if (index !== 1) {
-      container.style.transform = `translateX(${translateX + 600}px)`;
-      index--;
       translateX += 600;
+      container.style.transform = `translateX(${translateX}px)`;
+      index--;
     }
   };
 
   const nextSlide = () => {
     if (index < images.length) {
-      container.style.transform = `translateX(${translateX - 600}px)`;
-      index++;
       translateX -= 600;
+      container.style.transform = `translateX(${translateX}px)`;
+      index++;
     }
+  };
+
+  const scrollToImage = e => {
+    translateX = e.target.dataset.index * -600;
+    container.style.transform = `translateX(${translateX}px)`;
+    index = parseInt(e.target.dataset.id);
   };
 
   prevBtn.addEventListener('click', previousSlide);
@@ -27,30 +34,43 @@ const imageSlider = (() => {
 
   const images = [
     {
+      id: 1,
       title: 'image one',
       url: './images/img1.jpg',
     },
+    { id: 2, title: 'image two', url: './images/img2.jpg' },
     {
-      title: 'image two',
-      url: './images/img2.jpg',
-    },
-    {
+      id: 3,
       title: 'image three',
       url: './images/img3.jpg',
     },
     {
+      id: 4,
       title: 'image four',
       url: './images/img4.jpg',
     },
     {
+      id: 5,
       title: 'image five',
       url: './images/img5.jpg',
     },
   ];
 
-  images.forEach(image => {
+  images.map((image, index) => {
     container.innerHTML += `
       <img alt="${image.title}" src="${image.url}" width="600px"/>
     `;
+    navDotsContainer.innerHTML += `
+      <i class="fas fa-circle nav-dot" data-id="${image.id}" data-index="${index}""></i>
+    `;
+  });
+
+  const navDots = document.querySelectorAll('.nav-dot');
+  navDots.forEach(navDot => {
+    navDot.addEventListener('click', e => scrollToImage(e));
   });
 })();
+
+// add circle navigation
+
+// add auto advance
