f70db58e89de61eea1124a71c55d17890053ef3b
Andy Hall <andersonhall@gmail.com>
2021-05-29 19:37:45 -0500

    async await

--- 022-working-with-apis/script.js
+++ 022-working-with-apis/script.js
@@ -2,10 +2,10 @@ const img = document.querySelector('img');
 const btn = document.querySelector('button');
 const input = document.querySelector('input');
 
-const fetchGif = () => {
+const fetchGif = async () => {
   const searchTerm = input.value;
 
-  fetch(
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=Lp76GRxgFjYRlzBCJ1QDD3Wl9b64bvdC&s=${searchTerm}`,
     {
       mode: 'cors',
@@ -17,10 +17,11 @@ const fetchGif = () => {
     .then(function (response) {
       img.src = response.data.images.original.url;
     })
-    .catch((err, response) => {
+    .catch(err => {
       console.log(err);
       img.src = './default-img.gif';
     });
+  const data = await response.json();
 };
 
 btn.addEventListener('click', fetchGif);
