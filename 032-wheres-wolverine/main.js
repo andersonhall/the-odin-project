7baeb1b3061258ef0eafbb1587691e517fdfdc05
Andy Hall <andersonhall@gmail.com>
2021-06-16 22:45:57 -0500

    add dropdowns

--- 032-wheres-wolverine/main.js
+++ 032-wheres-wolverine/main.js
@@ -3,66 +3,103 @@ const Game = (() => {
 
   const img = document.querySelector('#bg');
   img.addEventListener('click', e => {
+    // save click pos
+    const y = e.pageY;
+    const x = e.pageX;
+
     // clean up old squares
     if (document.querySelector('.square')) {
-      const oldSqare = document.querySelector('.square');
-      oldSqare.remove();
+      const oldSquare = document.querySelector('.square');
+      oldSquare.remove();
+      const oldDropdown = document.querySelector('.drop-down');
+      oldDropdown.remove();
+    } else {
+      // create square around clicked area
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.style.top = `${e.pageY - 50}px`;
+      square.style.left = `${e.pageX - 50}px`;
+      square.style.position = 'absolute';
+      document.body.appendChild(square);
+
+      // create dropdown with character selection
+      const dropDown = document.createElement('ul');
+      dropDown.classList.add('drop-down');
+      dropDown.style.top = `${e.pageY + 50}px`;
+      dropDown.style.left = `${e.pageX - 50}px`;
+      dropDown.style.position = 'absolute';
+      dropDown.innerHTML = `
+      <ul><button class='find-btn wolverine-btn'>Wolverine</button></ul>
+      <ul><button class='find-btn hulk-btn'>Hulk</button></ul>
+      <ul><button class='find-btn spider-man-btn'>Spider-man</button></ul>
+    `;
+      document.body.appendChild(dropDown);
+
+      // set up button event listeners
+      const btns = document.querySelectorAll('.find-btn');
+      btns.forEach(btn => {
+        btn.addEventListener('click', e => checkAnswer(e.target.textContent));
+      });
+
+      // set character squares
+      const wolverineEl = document.querySelector('.wolverine');
+      const wolverine = {
+        top: wolverineEl.getBoundingClientRect().top + window.scrollY,
+        bottom: wolverineEl.getBoundingClientRect().bottom + window.scrollY,
+        left: wolverineEl.getBoundingClientRect().left + window.scrollX,
+        right: wolverineEl.getBoundingClientRect().right + window.scrollX,
+      };
+
+      const hulkEl = document.querySelector('.hulk');
+      const hulk = {
+        top: hulkEl.getBoundingClientRect().top + window.scrollY,
+        bottom: hulkEl.getBoundingClientRect().bottom + window.scrollY,
+        left: hulkEl.getBoundingClientRect().left + window.scrollX,
+        right: hulkEl.getBoundingClientRect().right + window.scrollX,
+      };
+
+      const spiderManEl = document.querySelector('.spider-man');
+      const spiderMan = {
+        top: spiderManEl.getBoundingClientRect().top + window.scrollY,
+        bottom: spiderManEl.getBoundingClientRect().bottom + window.scrollY,
+        left: spiderManEl.getBoundingClientRect().left + window.scrollX,
+        right: spiderManEl.getBoundingClientRect().right + window.scrollX,
+      };
+
+      const checkAnswer = characterName => {
+        const character = characterName.toLowerCase();
+
+        // check for found character
+        if (
+          character === 'wolverine' &&
+          x >= wolverine.left &&
+          x <= wolverine.right &&
+          y >= wolverine.top &&
+          y <= wolverine.bottom
+        ) {
+          found = 'wolverine';
+        } else if (
+          character === 'hulk' &&
+          x >= hulk.left &&
+          x <= hulk.right &&
+          y >= hulk.top &&
+          y <= hulk.bottom
+        ) {
+          found = 'hulk';
+        } else if (
+          character === 'spider-man' &&
+          x >= spiderMan.left &&
+          x <= spiderMan.right &&
+          y >= spiderMan.top &&
+          y <= spiderMan.bottom
+        ) {
+          found = 'spider-man';
+        }
+        if (found !== '') {
+          alert(`you found ${found}`);
+        }
+        found = '';
+      };
     }
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.top = `${e.pageY - 50}px`;
-    square.style.left = `${e.pageX - 50}px`;
-    square.style.position = 'absolute';
-    document.body.appendChild(square);
-    console.log(e.pageX, e.pageY);
-
-    const wolverineEl = document.querySelector('.wolverine');
-    const wolverine = {
-      top: wolverineEl.getBoundingClientRect().top + window.scrollY,
-      bottom: wolverineEl.getBoundingClientRect().bottom + window.scrollY,
-      left: wolverineEl.getBoundingClientRect().left + window.scrollX,
-      right: wolverineEl.getBoundingClientRect().right + window.scrollX,
-    };
-
-    const hulkEl = document.querySelector('.hulk');
-    const hulk = {
-      top: hulkEl.getBoundingClientRect().top + window.scrollY,
-      bottom: hulkEl.getBoundingClientRect().bottom + window.scrollY,
-      left: hulkEl.getBoundingClientRect().left + window.scrollX,
-      right: hulkEl.getBoundingClientRect().right + window.scrollX,
-    };
-
-    const spiderManEl = document.querySelector('.spider-man');
-    const spiderMan = {
-      top: spiderManEl.getBoundingClientRect().top + window.scrollY,
-      bottom: spiderManEl.getBoundingClientRect().bottom + window.scrollY,
-      left: spiderManEl.getBoundingClientRect().left + window.scrollX,
-      right: spiderManEl.getBoundingClientRect().right + window.scrollX,
-    };
-
-    if (
-      e.pageX >= wolverine.left &&
-      e.pageX <= wolverine.right &&
-      e.pageY >= wolverine.top &&
-      e.pageY <= wolverine.bottom
-    ) {
-      found = 'wolverine';
-    } else if (
-      e.pageX >= hulk.left &&
-      e.pageX <= hulk.right &&
-      e.pageY >= hulk.top &&
-      e.pageY <= hulk.bottom
-    ) {
-      found = 'hulk';
-    } else if (
-      e.pageX >= spiderMan.left &&
-      e.pageX <= spiderMan.right &&
-      e.pageY >= spiderMan.top &&
-      e.pageY <= spiderMan.bottom
-    ) {
-      found = 'spider-man';
-    }
-    console.log(found);
-    found = '';
   });
 })();
